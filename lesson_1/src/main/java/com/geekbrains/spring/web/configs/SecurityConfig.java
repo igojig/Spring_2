5e5b7e8e6c41db52ba642b0e3c5b0e4b01adeef9
igojig <igojig@mail.ru>
2023-01-30 18:00:02 +0300

    урок 1. базовый проект Фисунова

---
+++ lesson_1/src/main/java/com/geekbrains/spring/web/configs/SecurityConfig.java
@@ -0,0 +1,90 @@
+package com.geekbrains.spring.web.configs;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfig {
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(JwtRequestFilter filter, HttpSecurity security) throws Exception {
+
+        return security
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/orders/**").authenticated()
+                .requestMatchers("/api/v1/profile").authenticated()
+                .requestMatchers("/api/v1/users/**").hasAnyRole("ADMIN")
+                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/auth").permitAll()
+                .requestMatchers("/demo").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/api/v1/orders/**").authenticated()
+//                .antMatchers("/api/v1/profile").authenticated()
+//                .antMatchers("/h2-console/**").permitAll()
+//                .anyRequest().permitAll()
+//                .and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .headers().frameOptions().disable()
+//                .and()
+//                .exceptionHandling()
+//                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+//
+//        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+//    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationProvider... providers) {
+        return new ProviderManager(providers);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new StandardAuthenticationProvider();
+    }
+
+//    @Override
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+}
